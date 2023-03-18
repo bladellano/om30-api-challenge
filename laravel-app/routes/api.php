649f787e20ee6c02f8c59ae335845fb4e6bb5497
Caio <bladellano@gmail.com>
2023-03-18 10:28:52 -0300

    [OM-003] Adicionando rotas que existirão na aplicação

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CepController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\ImportCsvController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return 'Working...';
+});
+
+Route::resource('patients', PatientController::class);
+
+Route::get('cep/{cep}', [CepController::class,'get']);
+
+Route::post('import-csv', [ImportCsvController::class,'store']);
